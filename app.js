cf4e9c1e008ae5140ee6a00b6bc775e6da179ca9
Ali Malek <ali.malek.71@gmail.com>
2017-04-11 01:56:29 +0430

    add LogTimeRoute + 404 error handling + refactoring

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+// var favicon = require('serve-favicon')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+var errorMessages = require('./util/error-messages')
 
-var insideOut = require('./routes/inside-out');
+var insideOut = require('./routes/inside-out')
 
-var app = express();
+var app = express()
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/insideout', insideOut);
+app.use('/insideout', insideOut)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function (req, res, next) {
+  let err = new Error(errorMessages.not_found())
+  err.status = 404
+  next(err)
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  let error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
+  res.status(err.status || 500)
+  res.json({
+    status: {
+      code: error.status || 500,
+      message: error.message
+    }
+  })
+})
 
-module.exports = app;
+module.exports = app
