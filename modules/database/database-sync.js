5391dd4ed0d9c4b13f43bb337e43d3a53ddf42ff
Ali Malek <ali.malek.71@gmail.com>
2017-04-11 22:55:48 +0430

    add catch error to database sync

--- modules/database/database-sync.js
+++ modules/database/database-sync.js
@@ -2,5 +2,7 @@ let db = require('./database-init.js')
 // let models = require('./app')
 
 module.exports = () => {
-  db.sequelize.sync({}).then(() => console.log('db created'))
+  db.sequelize.sync({})
+    .then(() => console.log('db created'))
+    .catch(err => console.log(err))
 }
