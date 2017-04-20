afddb1345e780c745c8ef1a805068f6e72f3bfb1
Ali Malek <ali.malek.71@gmail.com>
2017-04-20 10:57:57 +0430

    change the url path name

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/insideout', insideOut)
+app.use('/', insideOut)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
